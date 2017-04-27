cd22c4195395b091470e2b1363835f5a5330182b
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-04-27 13:47:31 +0200

    Using an audio single reference

--- run-single-reference.sh
+++ run-single-reference.sh
@@ -3,4 +3,4 @@ source ./environment.sh
 
 HARVESTER_PATH="$BASEPATH/abstract-harvester-base/src/CHAOS/Harvester/ChaosHarvester.php"
 CONFIGURATION_PATH="$DIR/configurations/PSDBConfiguration.xml"
-php $HARVESTER_PATH --configuration=$CONFIGURATION_PATH --mode=single-reference --reference=urn:dr:mu:programcard:58f9f2c0a11fa0112c32e29d
+php $HARVESTER_PATH --configuration=$CONFIGURATION_PATH --mode=single-reference --reference=urn:dr:mu:programcard:5901ad3da11f9f112c7544e6 --debug
