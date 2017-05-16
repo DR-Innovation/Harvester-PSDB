73797e489469ebe50247de6bc614979a908d58c9
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-05-16 11:53:08 +0200

    Changed the single reference

--- run-single-reference.sh
+++ run-single-reference.sh
@@ -4,4 +4,4 @@ source ./environment.sh
 HARVESTER_PATH="$BASEPATH/abstract-harvester-base/src/CHAOS/Harvester/ChaosHarvester.php"
 CONFIGURATION_PATH="$DIR/configurations/PSDBConfiguration.xml"
 # php $HARVESTER_PATH --configuration=$CONFIGURATION_PATH --mode=single-reference --reference=urn:dr:mu:programcard:590065166187a414a0495c63 --debug --debug-metadata
-php $HARVESTER_PATH --configuration=$CONFIGURATION_PATH --mode=single-reference --reference=urn:dr:mu:programcard:58ec9fe1a11f9f152c8cc91d --debug --debug-metadata
+php $HARVESTER_PATH --configuration=$CONFIGURATION_PATH --mode=single-reference --reference=urn:dr:mu:programcard:5914202ca11f9f12ec2c0f9d --debug --debug-metadata
