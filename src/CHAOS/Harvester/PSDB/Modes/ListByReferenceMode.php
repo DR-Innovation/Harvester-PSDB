b4f104de68cad3b4a2edcb158da75681cd88b5d4
Arne Jørgensen <arne@arnested.dk>
2018-05-16 08:36:41 +0200

    Log TotalSize at info level
    So we get the total size logged also when not running in debug mode.

--- src/CHAOS/Harvester/PSDB/Modes/ListByReferenceMode.php
+++ src/CHAOS/Harvester/PSDB/Modes/ListByReferenceMode.php
@@ -23,7 +23,7 @@ class ListByReferenceMode extends SetByReferenceMode implements Loadable {
     $offset = 0;
     do {
       $list = $psdb->getList($reference, self::LIMIT, $offset);
-      $this->_harvester->debug('TotalSize of list: ' . $list->TotalSize);
+      $this->_harvester->info('TotalSize of list: ' . $list->TotalSize);
       $items = $list->Items;
       foreach($items as $item) {
         if($item->Type === 'ProgramCard') {
