07c23ff3e217cc69fb7efde4be62dfc36bd5af51
Hasse Wilson <hasse@ramlev.dk>
2023-11-10 09:04:41 +0100

    Handle non-existing thumbnails better, skip if 404

--- src/CHAOS/Harvester/PSDB/Processors/ItemThumbnailFileProcessor.php
+++ src/CHAOS/Harvester/PSDB/Processors/ItemThumbnailFileProcessor.php
@@ -2,18 +2,33 @@
 
 namespace CHAOS\Harvester\PSDB\Processors;
 
-use CHAOS\Harvester\Loadable;
 use CHAOS\Harvester\Processors\FileProcessor;
 use CHAOS\Harvester\Shadows\ObjectShadow;
 
 class ItemThumbnailFileProcessor extends FileProcessor {
 
   public function process(&$externalObject, &$shadow = null) {
-		// Precondition
-		assert($shadow instanceof ObjectShadow);
+    // Precondition
+    assert($shadow instanceof ObjectShadow);
+
+    $fileShadow = null;
+
     // Add the primary image as a file shadow
     $primary_image_url = $externalObject->PrimaryImageUri;
-    $fileShadow = $this->createFileShadowFromURL($primary_image_url);
+
+    // Check if image exists before adding as file shadow.
+    $cp = curl_init($primary_image_url);
+    curl_setopt($cp, CURLOPT_NOBODY, true);
+    $result = curl_exec($cp);
+
+    if (false !== $result) {
+      $statusCode = curl_getinfo($cp, CURLINFO_HTTP_CODE);
+
+      if (404 !== $statusCode) {
+        $fileShadow = $this->createFileShadowFromURL($primary_image_url);
+      }
+    }
+
     $shadow->fileShadows[] = $fileShadow;
   }
 }
